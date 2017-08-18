818c481e4490b7b5706bee102abe0f2595a49003
Manu Mtz.-Almeida <manu.valladolid@gmail.com>
2017-08-18 16:08:10 +0200

    Minor changes

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 Go-client allows to easily comunicate with the Babelfish server to parse the any source code.
 It's also integrated with libuast so it is possible to run xpath queries agains the UAST in an idiomatic way.
+
+
+
+## Installation
+
+### Dependencies
+
+- libuast
+
+### Go library
+```
+go get github.com/bblfsh/client-go
+```
+
+
+
--- client.go
+++ client.go
@@ -24,15 +24,3 @@ func NewBblfshClient(endpoint string) (*BblfshClient, error) {
 func (c *BblfshClient) Parse() *ParseRequest {
 	return &ParseRequest{client: c}
 }
-
-// import "github.com/bblfsh/go-client"
-
-// func main() {
-// 	client, _ := bblfsh.NewBblfshClient("0.0.0.0:9413")
-// 	root, _ := client.Parse().ReadFile("hola").Do()
-// 	// 	root, _ := client.Parse().Content("C = A+B").Language("Python").Do()
-
-// 	for _, node := range bblfsh.Find(root, "//Module/*") {
-// 		fmt.Println(node)
-// 	}
-// }
