875a2e88e1c3cf78bbbb9819564e1f1821bdb5a3
Máximo Cuadros <mcuadros@gmail.com>
2017-09-13 12:50:53 +0200

    gopkg.in enforcement

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Babelfish (https://doc.bblf.sh) Go client library provides functionality to
+// both connect to the Babelfish server to parse code (obtaining an UAST as a
+// result) and to analyse UASTs with the functionality provided by libuast.
+package bblfsh // import "gopkg.in/bblfsh/client-go.v0"
