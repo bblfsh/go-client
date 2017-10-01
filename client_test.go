f1c0fdaa332a96511471b3dd41066e23c1a1767b
Máximo Cuadros <mcuadros@gmail.com>
2017-10-02 01:45:18 +0200

    clients: added new methods native and version requests and client renamed

--- client_test.go
+++ client_test.go
@@ -1,29 +1,52 @@
 package bblfsh
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestClient(t *testing.T) {
+func TestClient_NewParseRequest(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
 
-	cli, err := NewBblfshClient("localhost:9432")
+	cli, err := NewClient("localhost:9432")
 	require.Nil(t, err)
 
-	python := "import foo"
-
-	res, err := cli.NewParseRequest().Language("python").Content(python).Do()
-	require.Nil(t, err)
-
-	fmt.Println(res.Errors)
-	fmt.Println(res.UAST)
+	res, err := cli.NewParseRequest().Language("python").Content("import foo").Do()
+	require.NoError(t, err)
 
 	require.Equal(t, len(res.Errors), 0)
 	require.NotNil(t, res.UAST)
+}
 
+func TestClient_NewNativeParseRequest(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	cli, err := NewClient("localhost:9432")
+	require.Nil(t, err)
+
+	res, err := cli.NewNativeParseRequest().Language("python").Content("import foo").Do()
+	require.NoError(t, err)
+
+	require.Equal(t, len(res.Errors), 0)
+	require.NotNil(t, res.AST)
+}
+
+func TestClient_NewVersionRequest(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	cli, err := NewClient("localhost:9432")
+	require.Nil(t, err)
+
+	res, err := cli.NewVersionRequest().Do()
+	require.NoError(t, err)
+
+	require.Equal(t, len(res.Errors), 0)
+	require.NotNil(t, res.Version)
 }
